24b6cfca1cd1a41c837cd1c51d56dc742d21277d
Sahil Tendulkar <sahiltendulkar107@gmail.com>
2026-03-28 01:04:47 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BillingAndInvoiceSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
