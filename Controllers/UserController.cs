abf2cfa2ba2c80a7625a52d6bee0886fa0bc7d71
Sahil Tendulkar <sahiltendulkar107@gmail.com>
2026-03-31 14:00:16 +0530

    BS-3 Completed Registration Module with Database Integration

---
+++ Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using BillingAndInvoiceSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillingAndInvoiceSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Register Page
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Save User
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+
+                return RedirectToAction("Login");
+            }
+
+            return View(user);
+        }
+
+        // Login Page (temporary)
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
